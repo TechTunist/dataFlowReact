31eb3d6bc8e97edb5d21738347b5903dee5f7d4c
tunist <mrmpjackson@gmail.com>
2024-05-12 20:47:35 +0100

    test failed for touchscreens

--- src/components/AltcoinPrice.jsx
+++ src/components/AltcoinPrice.jsx
@@ -457,28 +457,19 @@ const AltcoinPrice = ({ isDashboard = false }) => {
                     }
                 </div>              
             </div>
-            <div className="chart-container"
-                style={{ 
+            <div className="chart-container" style={{ 
                     position: 'relative', 
                     height: 'calc(100% - 40px)', 
                     width: '100%', 
                     border: '2px solid #a9a9a9' // Adds dark border with your specified color
-                }}
-                onClick={() => {
-                    if (!isInteractive && !isDashboard) {  // Only set interactivity if it's currently disabled
-                        setInteractivity();
-                    }
-                }}
-                onTouchEnd={(e) => {
-                    // Prevent firing the event twice on devices that recognize both touch and click events
-                    e.preventDefault();
-                    if (!isInteractive && !isDashboard) {
-                        setInteractivity();
-                    }
-                }}>
+                    }}
+                    onClick={() => {
+                        if (!isInteractive && !isDashboard) {  // Only set interactivity if it's currently disabled
+                            setInteractivity();
+                        }
+                    }}>                
                 <div ref={chartContainerRef} style={{ height: '100%', width: '100%', zIndex: 1 }} />
             </div>
-
             {!isDashboard && (
                 <LastUpdated storageKey="btcData" />
             )}
