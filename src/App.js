e9d393bec3181d29aaa3000f14a5741c40c49cd3
tunist <mrmpjackson@gmail.com>
2025-06-18 13:26:38 +0100

    average on chain risk no weighted option. btcmvrv no z score or realised price or sidebar link

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ import MonthlyAverageROI from "./components/MonthlyAverageROI";
 import Bitcoin20WeekExtension from "./components/Bitcoin20WeekExtension";
 import HistoricalVolatility from "./components/HistoricalVolatility";
 import PuellMultiple from "./components/PuellMultiple";
+import BitcoinMvrvChart from "./components/BitcoinMvrv";
 
 // Stripe Context
 const StripeContext = createContext(null);
@@ -519,6 +520,14 @@ const AppContent = memo(({ isSignedIn, user }) => {
                       element={
                         <ProtectedRoute>
                           <BasicChart ChartComponent={BitcoinTxMvrvChart} />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/mvrv"
+                      element={
+                        <ProtectedRoute>
+                          <BasicChart ChartComponent={BitcoinMvrvChart} />
                         </ProtectedRoute>
                       }
                     />
